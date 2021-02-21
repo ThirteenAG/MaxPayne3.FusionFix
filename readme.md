7d946289130182846c1c5e3d1d75dcd760342512
ThirteenAG <thirteenag@gmail.com>
2021-02-21 21:09:26 +0300

    initial release

---
+++ readme.md
@@ -0,0 +1,26 @@
+[![Actions Status: Release](https://github.com/ThirteenAG/MaxPayne3.FusionFix/actions/workflows/msvc_x86.yml/badge.svg)](https://github.com/ThirteenAG/MaxPayne3.FusionFix/actions) [![Discord](https://img.shields.io/badge/chat-on%20discord-7289da.svg?logo=discord)](https://discord.gg/RaZXpKExNj)
+
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/4904157/108633054-2ed23d00-7483-11eb-8b6a-2d04e1ac138d.png">
+</p>
+
+# MaxPayne3.FusionFix
+
+This is a small project intended to add ability to hide  ![skip](https://i.imgur.com/vwELI93.png)  button in Max Payne 3.
+Additionally, added an option to increase the size of subtitle text outlines.
+
+## Installation:
+
+### **Download**: [MaxPayne3.FusionFix.zip](https://github.com/ThirteenAG/MaxPayne3.FusionFix/releases/latest/download/MaxPayne3.FusionFix.zip)
+
+This requires [ASI Loader](https://github.com/ThirteenAG/Ultimate-ASI-Loader/releases/latest/download/Ultimate-ASI-Loader.zip).
+
+Unpack the content of archives to your **Max Payne 3** directory.
+
+## Screenshots:
+
+Before:
+![](https://i.imgur.com/IOqRXKR.jpg)
+
+After:
+![](https://i.imgur.com/28KZ32b.jpg)
---
+++ source/dllmain.cpp
@@ -0,0 +1,67 @@
+#include <misc.h>
+
+float fOutlinesSizeMultiplier;
+float* dword_1FCA9A8;
+void __fastcall sub_010AFD30(float* _this, void* edx, float a2)
+{
+    a2 *= fOutlinesSizeMultiplier;
+
+    if (a2 <= 0.0)
+    {
+        _this[8] = 0.0;
+        _this[13] = 0.0;
+    }
+    else
+    {
+        _this[8] = a2 / *dword_1FCA9A8;
+        _this[13] = a2 / *dword_1FCA9A8;
+    }
+}
+
+void Init()
+{
+    CIniReader iniReader("");
+    bool bHideSkipButton = iniReader.ReadInteger("MAIN", "HideSkipButton", 1) != 0;
+    fOutlinesSizeMultiplier = iniReader.ReadFloat("MAIN", "OutlinesSizeMultiplier", 0.0f);
+
+    if (bHideSkipButton)
+    {
+        auto pattern = hook::pattern("8B C8 89 86 ? ? ? ? E8 ? ? ? ? D9 EE 8B 86 ? ? ? ? 8B 38 83 EC 08");
+        injector::MakeNOP(pattern.count(22).get(0).get<void>(8), 5, true);
+        injector::MakeNOP(pattern.count(22).get(3).get<void>(8), 5, true);
+    }
+
+    if (fOutlinesSizeMultiplier)
+    {
+        dword_1FCA9A8 = *hook::get_pattern<float*>("F3 0F 10 86 ? ? ? ? F3 0F 59 05 ? ? ? ? 51 8D 8C 24", 12);
+
+        auto pattern = hook::pattern("E8 ? ? ? ? 68 ? ? ? ? 8D 94 24 ? ? ? ? 6A 02");
+        injector::MakeCALL(pattern.get_first(0), sub_010AFD30, true);
+
+        pattern = hook::pattern("D9 1C 24 E8 ? ? ? ? 68 ? ? ? ? 68");
+        injector::MakeCALL(pattern.get_first(3), sub_010AFD30, true);
+
+        pattern = hook::pattern("E8 ? ? ? ? 68 ? ? ? ? 8D 44 24 20 6A 03 50 88 1D ? ? ? ? C6 05 ? ? ? ? ? 88 1D ? ? ? ? E8 ? ? ? ? 8B 08");
+        injector::MakeCALL(pattern.get_first(0), sub_010AFD30, true);
+
+        pattern = hook::pattern("E8 ? ? ? ? F3 0F 10 44 24 ? 8D 4C 24 30 F3 0F 11 05 ? ? ? ? F3 0F 10 44 24 ? 51 B9 ? ? ? ? F3 0F 11 05 ? ? ? ? E8 ? ? ? ? 8A 5C 24 0F 80 FB FF 75 0C");
+        injector::MakeCALL(pattern.get_first(0), sub_010AFD30, true);
+    }
+}
+
+CEXP void InitializeASI()
+{
+    std::call_once(CallbackHandler::flag, []()
+    {
+        CallbackHandler::RegisterCallback(Init, hook::pattern("C6 44 24 ? ? F3 0F 11 44 24 ? F3 0F 11 44 24 ? 89 4C 24 54"));
+    });
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
+{
+    if (reason == DLL_PROCESS_ATTACH)
+    {
+        if (!IsUALPresent()) { InitializeASI(); }
+    }
+    return TRUE;
+}
