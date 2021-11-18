585fe11c5ac3a6ec98590ac0369aa688f217dd60
ThirteenAG <thirteenag@gmail.com>
2021-11-18 04:37:44 +0300

    fix for updated version - fixes #1 - fixes #2

--- source/dllmain.cpp
+++ source/dllmain.cpp
@@ -26,9 +26,10 @@ void Init()
 
     if (bHideSkipButton)
     {
-        auto pattern = hook::pattern("8B C8 89 86 ? ? ? ? E8 ? ? ? ? D9 EE 8B 86 ? ? ? ? 8B 38 83 EC 08");
-        injector::MakeNOP(pattern.count(22).get(0).get<void>(8), 5, true);
-        injector::MakeNOP(pattern.count(22).get(3).get<void>(8), 5, true);
+        auto pattern = hook::pattern("8B C8 89 86 ? ? ? ? E8 ? ? ? ? D9 EE 8B 86 ? ? ? ? 8B 38 83 EC 08 D9 54 24 04 8D 4C 24 3C D9 1C 24 E8 ? ? ? ? F3 0F 7E 00 8B 97 ? ? ? ? 83 EC 18 8B CC 66 0F D6 01 F3 0F 7E 40 ? 66 0F D6 41 ? F3 0F 7E 40 ? 6A 18 68");
+        injector::MakeNOP(pattern.get_first(8), 5, true);
+        pattern = hook::pattern("8B C8 89 86 ? ? ? ? E8 ? ? ? ? D9 EE 8B 86 ? ? ? ? 8B 38 83 EC 08 D9 54 24 04 8D 4C 24 20 D9 1C 24 E8 ? ? ? ? F3 0F 7E 00 8B 97 ? ? ? ? 83 EC 18 8B CC 66 0F D6 01 F3 0F 7E 40 ? 66 0F D6 41 ? F3 0F 7E 40 ? 6A 18 68 ? ? ? ? 66 0F D6 41 ? 8B 8E ? ? ? ? 6A 18");
+        injector::MakeNOP(pattern.get_first(8), 5, true);
     }
 
     if (fOutlinesSizeMultiplier)
