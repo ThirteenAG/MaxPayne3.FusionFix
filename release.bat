40f3347ac8ef2dab726834e2382818ab1a7e2271
ThirteenAG <thirteenag@gmail.com>
2026-04-03 14:23:57 +0300

    add dxwrapper for FixHighFrequencyMouse

--- release.bat
+++ release.bat
@@ -1,3 +1,11 @@
+for /f "tokens=1,* delims=:" %%A in ('curl -ks https://api.github.com/repos/elishacloud/dxwrapper/releases/latest ^| find "browser_download_url"') do (
+  echo.%%B | FIND /I "dxwrapper">Nul && echo.%%B | FIND /I "debug">Nul || (
+    curl -o dxwrapper.zip -kL %%B
+  )
+)
+
+7z e "dxwrapper.zip" "dxwrapper.asi" -o".\data\plugins" -y
+
 call tools\EmbedPDB\EmbedPDB.exe bin\MaxPayne3.FusionFix.asi
 
 powershell -NoProfile -ExecutionPolicy Bypass -File "Sign.ps1" -SearchPaths ".\bin\MaxPayne3.FusionFix.asi"
